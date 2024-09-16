2014a27cc6018b399db6131dbd9d83f8d3102325
Heisenberg <esfahani.cyrus@gmail.com>
2024-09-16 09:06:00 -0700

    update the AboutME file and added content

--- Portfolio/src/components/aboutme.jsx
+++ Portfolio/src/components/aboutme.jsx
@@ -1,3 +1,14 @@
 import React from 'react'
 
-const AboutMe = 8
+const AboutMe = () => {
+  return (
+    <section id="about">
+      <h2>About Me</h2>
+      <p>
+        Hi, I'm Cyrus! I'm a software engineer with a passion for web development. I'm experienced in JavaScript, React, and Node.js, and I'm always looking to learn more. I'm currently seeking new opportunities, so feel free to reach out!
+      </p>
+    </section>
+  )
+}
+
+export default AboutMe

7f7dbe1231e4415a13b1370cd18a5b42182e6eb8
Heisenberg <esfahani.cyrus@gmail.com>
2024-09-24 18:35:36 -0700

    added more css styling to our page

--- Portfolio/src/App.jsx
+++ Portfolio/src/App.jsx
@@ -2,6 +2,8 @@ import Navigation from './components/Navigation.jsx'
 import { Outlet } from 'react-router-dom'
 import Header from './components/header.jsx'
 import Footer from './components/Footer.jsx'
+import contact from './components/Contact.jsx'
+import './App.css'
 
 
 function App() {
--- Portfolio/src/components/Navigation.jsx
+++ Portfolio/src/components/Navigation.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom'
+import '../styles/header.css'
 
 export default function Navigation () {
     return (

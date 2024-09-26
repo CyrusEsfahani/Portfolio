10a7acd2fdddbb018758d78303f90e89a3a90818
Heisenberg <esfahani.cyrus@gmail.com>
2024-09-25 23:09:12 -0700

    added some parllax function and smooth scrolling to my page

--- Portfolio/src/components/Navigation.jsx
+++ Portfolio/src/components/Navigation.jsx
@@ -1,18 +1,47 @@
-import React from 'react'
-import { NavLink } from 'react-router-dom'
-import '../styles/header.css'
+import React from "react";
+// import { NavLink } from 'react-router-dom'
+import "../styles/header.css";
+import { Link } from "react-scroll";
 
-export default function Navigation () {
-    return (
-        
-        <nav>
-          <NavLink to="/" className= {({isActive}) => isActive? "active":"link"}>About Me</NavLink>
-          <NavLink to="/portfolio" className= {({isActive}) => isActive? "active":"link"}>Porfolio</NavLink>
-          <NavLink to="/resume" className= {({isActive}) => isActive? "active":"link"}>Resume</NavLink>
-          <NavLink to="/contact" className= {({isActive}) => isActive? "active":"link"}>Contact Me</NavLink>
-        </nav>
-      
-    )
-    }
-
-   
+export default function Navigation() {
+  return (
+    <nav>
+      <Link
+        to="/"
+        smooth={true}
+        duration={500}
+        className="link"
+        activeClass="active"
+      >
+        About Me
+      </Link>
+      <Link
+        to="portfolio"
+        smooth={true}
+        duration={500}
+        className="link"
+        activeClass="active"
+      >
+        Portfolio
+      </Link>
+      <Link
+        to="resume"
+        smooth={true}
+        duration={500}
+        className="link"
+        activeClass="active"
+      >
+        Resume
+      </Link>
+      <Link
+        to="contact"
+        smooth={true}
+        duration={500}
+        className="link"
+        activeClass="active"
+      >
+        Contact
+      </Link>
+    </nav>
+  );
+}

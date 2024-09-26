20f2e48e5c7758aea1f1a939f93cdb51ac0d323e
Heisenberg <esfahani.cyrus@gmail.com>
2024-09-26 08:08:10 -0700

    added more styles to my page

--- Portfolio/src/components/Footer.jsx
+++ Portfolio/src/components/Footer.jsx
@@ -1,22 +1,27 @@
 import React from "react";
+import { FaLinkedin, FaGithub, FaTwitter } from 'react-icons/fa'
 import "../styles/footer.css";
 
 
-export default function Footer() {
+const Footer = () => {
   return (
-    <section>
-      <h2>footer</h2>
-      <a href="https://github.com/CyrusEsfahani" target="_blank">
-        GitHub
-      </a>
-      <a href="https://www.linkedin.com/in/cyrus-esfahani-261013225/" target="_blank">
-        LinkedIn
-      </a>
-      <a href="https://stackoverflow.com/users/developer" target="_blank">
-        Stack Overflow
-      </a>
-    </section>
-  );
+    <footer>
+      <h2>Connect with me</h2>
+      <div className="social-icons">
+        <a href="https://www.linkedin.com/in/cyrus-esfahani-261013225/" target="_blank" rel="noopener noreferrer">
+          <FaLinkedin size={30} />
+        </a>
+        <a href="https://github.com/CyrusEsfahani" target="_blank" rel="noopener noreferrer">
+          <FaGithub size={30} />
+        </a>
+        <a href="https://twitter.com/yourprofile" target="_blank" rel="noopener noreferrer">
+          <FaTwitter size={30} />
+        </a>
+      </div>
+    </footer>
+  )
 }
+
+export default Footer
 
 
